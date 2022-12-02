17c2229dc686c4bca74024c761e704476d16c687
rckannan <chinnakannanr@gmail.com>
2022-12-02 05:31:11 +0000

    Update 6 files

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Inherident } from '../shared/inherident.model';
 
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
-  styleUrls: ['./shoping-list.component.css']
+  styleUrls: ['./shoping-list.component.css'],
 })
 export class ShopingListComponent {
-
+  ingredients = [
+    new Inherident('inherid 1', 100),
+    new Inherident('inherid 2', 120),
+    new Inherident('inherid 3', 130),
+  ];
 }
